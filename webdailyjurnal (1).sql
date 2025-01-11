92a6f9c0fb112243e58a960d8a644b182b6a03ac
tiara1210 <111202315347@mhs.dinus.ac.id>
2025-01-11 11:20:47 +0700

    Update webdailyjurnal (1).sql

--- webdailyjurnal (1).sql
+++ webdailyjurnal (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2024 at 05:03 PM
+-- Generation Time: Jan 10, 2025 at 05:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,13 +41,38 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
-(1, 'Bandara Soekarno Hatta', 'Dari sini mulai perjalanan liburan kami ke bangka belitung dari Bandara Soekarno Hatta ke Bandara Tanjung Pandang dengan waktu tempuh 1 jam dengan menggunakan jalur udara dan pada saat penerbangan kami disambut dengan sunrise yang indah dan pemandangan timah yang berada dipulau bangka yang kami lihat saat penerbangan.', 'pesawat.jpg', '2024-08-07 05:52:37', 'admin'),
 (2, 'Bakmi Atep', 'Sesampainya dibandara Tanjung Pandang, kami bergegas untuk berkuliner dengan memakan bakmi tradisional khas Belitung yang sudah berdiri sejak 1973. yang mempunyai karakter dengan beralas daun simpur dengan kuah sedikit kental dengan perpaduan kaldu udang yang membuat bakmi semakin nikmmat.', 'bakmi.jpg', '2024-08-07 08:24:12', 'admin'),
 (3, 'Vihara Dewi Kwan Im', 'Vihara ini meonjolkan khas Tionghoa yang didominasi warna merah. ornamen lampion yang menghiasi langit-langit menambah keindahan vihara yang berusia lebih dari 250 tahun ini. vihara ini juga merupakan vihara terbesar dan tertua di Pulau Belitung, untuk mencapai kuil pengunjung harus menaiki sekitar 86 anak tangga.', 'vihara.jpg', '2024-08-07 11:26:58', 'admin'),
 (4, 'Replika Sekolah Laskar Pelangi', 'Ini adalah destinasi ke tiga, dimana pada saat sampai kami disambut dengan anak-anak yang berada diruang kelas yang menyanyikan lagu \"Laskar Pelangi\" dengan irama yang merdu dan membuat pengunjung speachless begitu indahnya tempat ini.', 'laskar.jpg', '2024-08-07 14:56:37', 'admin'),
-(5, 'Pantai Tanjung Tinggi', 'Pantai Tanjung Tinggi memiliki hamparan pasir putih yang lebut dan air laut berwarna hijau tosca yang sangat jernih. Kawasan pantai juga memiliki batu granitraksasa yang sangat ikonik.disini pengunjung dapat berfoto diantara batu granit,sunset,jetski,snorkeling,dll.', 'pantai.jpg', '2024-08-08 09:02:24', 'admin'),
-(6, 'keliling pulau', 'Pada hari ke dua kami berkeliling berbagai pulau yang berada di kepulauan belitung selama 1 harian full, dan tidak lupa juga kami berkunjung ke salah satu goa yang berada di suatu pulau kemudian kami berkunjung ke mercusuar dan setelah itu kami melakukan kegiatan snorkeling.', 'round.jpg', '2024-08-08 10:34:48', 'admin'),
-(8, 'Goa', 'keliling goa seru!!', '20241217225730.jpg', '2024-12-17 22:57:30', 'admin');
+(12, 'Diving', 'Semakin dalam kamu menyelam, semakin banyak keajaiban yang kamu temukan', '20250110014046.jpg', '2025-01-10 01:40:46', 'admin'),
+(16, 'Pulau Bangka Belitung', 'Dari pulau ke pulau, menjelajahi setiap sudut keajaiban Bangka Belitung #IslandHopping', '20250110014203.jpg', '2025-01-10 01:42:14', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL,
+  `judul` text DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `gambar` text DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `judul`, `tanggal`, `gambar`, `username`) VALUES
+(1, 'On The Way', '2025-01-09', 'ara.jpg', 'admin'),
+(2, 'laskar pelangi', '2025-01-09', 'laskar.jpg', 'admin'),
+(3, 'pantai', '2024-08-07', 'pantai.jpg', 'admin'),
+(4, 'keliling', '2025-01-09', 'round.jpg', 'admin'),
+(6, 'jalan', '2025-01-10', '20250110012150.jpg', 'admin'),
+(7, 'relax', '2025-01-10', '20250110012224.jpg', 'admin'),
+(8, 'berenang', '2025-01-10', '20250110012338.jpg', 'admin');
 
 -- --------------------------------------------------------
 
@@ -67,7 +92,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '');
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '20250110213304.jpg'),
+(3, 'tiara', 'fd6ddcb825c3bb7527cc76cd8ca4f701', '20250110213138.jpg'),
+(4, 'danny', '21232f297a57a5a743894a0e4a801fc3', '20250110213412.jpg'),
+(5, 'maudy', '16f5961ff1bad5a8a65175dfe421691e', '20250110214236.jpg'),
+(6, 'chelsi', '2c0c1895429ec2d08e6eaf13f1c05232', '20250110214340.jpg'),
+(7, 'michael', '9e6663e0097de65f795c9d080dea4975', '20250110215441.jpg'),
+(8, 'fabiola', '601fec56e1572ca81aaec9e3dad86c07', '20250110215034.jpg'),
+(9, 'khalifah', '9fd2a632d3a8656bb459d094ef9ba3be', '20250110215336.jpg');
 
 --
 -- Indexes for dumped tables
@@ -77,6 +109,12 @@ INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
 -- Indexes for table `article`
 --
 ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -93,13 +131,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
